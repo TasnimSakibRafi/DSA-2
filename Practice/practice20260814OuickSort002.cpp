e2b3a8f045352c128c0cdc1107d095bff8634351
ForestByte <tasnimsakibrafi@gmail.com>
2026-08-14 02:48:26 +0600

    Add quicksort implementation in practice20260814OuickSort002.cpp
    Implement quicksort algorithm to sort a vector of integers.

---
+++ Practice/practice20260814OuickSort002.cpp
@@ -0,0 +1,28 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+void quickSort (vector<int>&v,int s,int e){
+    if (s>=e) return ;
+    int pivot=v[e];
+    int i=s;
+    for (int j=s;j<e;j++){
+        if (v[j]>pivot ){
+            swap(v[i],v[j]);
+            i++;
+        }
+    }
+    swap(v[i],v[e]);
+    quickSort(v,s,i-1);
+    quickSort(v,i+1,e);
+}
+
+int main(){
+    vector<int>v={34,468,6,-7,3,-435,4353,2,-4353,46463};
+    quickSort(v,0,v.size()-1);
+    for (int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+        // i++;
+    }
+
+    return 0;
+}
