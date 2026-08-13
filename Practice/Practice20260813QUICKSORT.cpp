fd85c0575dfb9bfbd34d9bb2a5d805da04c9010f
ForestByte <tasnimsakibrafi@gmail.com>
2026-08-13 20:36:59 +0600

    Add quicksort implementation in Practice20260813QUICKSORT.cpp
    Implemented the quicksort algorithm for sorting integers in ascending order.

---
+++ Practice/Practice20260813QUICKSORT.cpp
@@ -0,0 +1,35 @@
+//This is for Acending Order 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void quickSort(vector<int>& v, int s, int e) {
+    if (s >= e) return;
+
+    // Inline partitioning
+    int pivot = v[e];   // choose last element as pivot
+    int i = s;          // index for smaller elements
+    for (int j = s; j < e; j++) {
+        if (v[j] < pivot) {
+            swap(v[i], v[j]);
+            i++;
+        }
+    }
+    swap(v[i], v[e]);   // place pivot in correct position
+
+    // Recursive calls
+    quickSort(v, s, i - 1);
+    quickSort(v, i + 1, e);
+}
+
+int main() {
+    vector<int> v = {3, 2, -60, 20, 3, 10, -2};
+    quickSort(v, 0, v.size() - 1);
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<< " ";
+    }
+    cout << endl;
+
+    return 0;
